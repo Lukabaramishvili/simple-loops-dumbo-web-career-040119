4a6f2ac7cf509de93dc4a7559772246130fe2918
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-03-18 14:50:14 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -7,7 +7,7 @@ def loop_iterator(number_of_times)
   loop do
     puts phrase 
     number_of_times += 1
-    if number_of_times >= 7
+    if number_of_times > 7
       break
     end
 end
@@ -33,7 +33,7 @@ def until_iterator(number_of_times)
 
   phrase = "Welcome to Flatiron School's Web Development Course!"
   number_of_times = 0
-  until number_of_times == 7
+  until number_of_times == 8
   puts phrase
   number_of_times += 1
   end
